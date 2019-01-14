733294c022b1f9a9d9f57cbcafd58c546b382860
Ethan Roberts <ejrobe@gmail.com>
2019-01-14 01:25:01 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,7 +20,7 @@ def word_substituter(tweet)
   # arr.to_s
   arr = tweet.split(" ").to_a
   new_arr = arr.collect do |each|
-    if words.keys.include?(each.downcase)
+    if words.keys.include?(each)
       each = words.fetch(each)
     else
       each
